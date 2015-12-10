5d809995cb00d90042545f50cb6070c6a39a7c2f
Dror Gluska <drorgl@yahoo.com>
2015-12-10 22:15:09 +0200

    fix build scripts. android uses the nodejs source dir instead of node-gyp's headers. linux uses x64 windows adds build

--- build_windows_alvision.bat
+++ build_windows_alvision.bat
@@ -1,1 +1,1 @@
-node-gyp configure -Dos_posix=0 -Dmsan=0 -Dclang=0 -Duse_system_yasm=0 -Dbuildtype=debug
+%NODE_GYP_PATH% configure -Dos_posix=0 -Dmsan=0 -Dclang=0 -Duse_system_yasm=0 -Dbuildtype=debug build
