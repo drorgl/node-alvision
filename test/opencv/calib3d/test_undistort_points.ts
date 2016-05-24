ae93fd82b1dd5ecc5e658344cf6573b5b05f2060
Dror Gluska <drorgl@yahoo.com>
2016-05-24 13:23:00 +0300

    test_undistort_points TS API ready for first test

--- test/opencv/calib3d/test_undistort_points.ts
+++ test/opencv/calib3d/test_undistort_points.ts
@@ -1,4 +1,3 @@
-
 import tape = require("tape");
 import path = require("path");
 import colors = require("colors");
@@ -61,14 +60,18 @@ class CV_UndistortTest extends alvision.cvtest.BaseTest {
     private generate3DPointCloud(points: Array<alvision.Point3f>, pmin: alvision.Point3f = new alvision.Point3f(-1, -1, 5), pmax: alvision.Point3f = new alvision.Point3f(1, 1, 10)): void {
         const delta = alvision.Point3f.op_Substraction(pmax, pmin);
         
+        
+
         for (var i = 0; i < points.length; i++)
         {
-            Point3f p(float(rand()) / RAND_MAX, float(rand()) / RAND_MAX,
-                float(rand()) / RAND_MAX);
-            p.x *= delta.x;
-            p.y *= delta.y;
-            p.z *= delta.z;
-            p = p + pmin;
+            var p = new alvision.Point3f(
+                this.rng.float().valueOf() / 0x7fff,
+                this.rng.float().valueOf() / 0x7fff,
+                this.rng.float().valueOf()/ 0x7fff);
+            p.x =p.x.valueOf()  * delta.x.valueOf();
+            p.y =p.y.valueOf()  * delta.y.valueOf();
+            p.z =p.z.valueOf()  * delta.z.valueOf();
+            p = p.op_Addition( pmin);
             points[i] = p;
         }
     }
@@ -76,18 +79,18 @@ class CV_UndistortTest extends alvision.cvtest.BaseTest {
         const fcMinVal = 1e-3;
         const fcMaxVal = 100;
         cameraMatrix.create(3, 3, alvision.MatrixType.CV_64FC1);
-        cameraMatrix.setTo(Scalar(0));
-        cameraMatrix.at<double>(0, 0) = rng.uniform(fcMinVal, fcMaxVal);
-        cameraMatrix.at<double>(1, 1) = rng.uniform(fcMinVal, fcMaxVal);
-        cameraMatrix.at<double>(0, 2) = rng.uniform(fcMinVal, fcMaxVal);
-        cameraMatrix.at<double>(1, 2) = rng.uniform(fcMinVal, fcMaxVal);
-        cameraMatrix.at<double>(2, 2) = 1;
+        cameraMatrix.setTo(new alvision.Scalar(0));
+        cameraMatrix.at<alvision.double>("double",0, 0).set( this.rng.uniform(fcMinVal, fcMaxVal));
+        cameraMatrix.at<alvision.double>("double",1, 1).set( this.rng.uniform(fcMinVal, fcMaxVal));
+        cameraMatrix.at<alvision.double>("double",0, 2).set( this.rng.uniform(fcMinVal, fcMaxVal));
+        cameraMatrix.at<alvision.double>("double",1, 2).set( this.rng.uniform(fcMinVal, fcMaxVal));
+        cameraMatrix.at<alvision.double>("double", 2, 2).set(1);
     }
     private generateDistCoeffs(distCoeffs : alvision.Mat, count : alvision.int): void {
         //DROR: should not work, overriding reference?
-        distCoeffs = alvision.Mat.zeros(count, 1, alvision.MatrixType.CV_64FC1);
+        distCoeffs = alvision.Mat.zeros(count, 1, alvision.MatrixType.CV_64FC1).toMat();
         for (var i = 0; i < count; i++)
-        distCoeffs.at<double>(i, 0) = rng.uniform(0.0, 1.0e-3);
+            distCoeffs.at<alvision.double>("double", i, 0).set(this.rng.uniform(0.0, 1.0e-3));
     }
 
     private  thresh: alvision.double;
