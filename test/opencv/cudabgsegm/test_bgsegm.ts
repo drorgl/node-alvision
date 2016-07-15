4084c7d09b5d7dd80e529cae581e2ce7c588655a
Dror Gluska <drorgl@yahoo.com>
2016-07-15 16:58:40 +0300

    test_bgsegm TS API ready for first test

--- test/opencv/cudabgsegm/test_bgsegm.ts
+++ test/opencv/cudabgsegm/test_bgsegm.ts
@@ -47,145 +47,150 @@ import async = require("async");
 import alvision = require("../../../tsbinding/alvision");
 import util = require('util');
 import fs = require('fs');
-
-#include "test_precomp.hpp"
-
-#ifdef HAVE_CUDA
-
-using namespace cvtest;
-
-#if defined(HAVE_XINE)         || \
-    defined(HAVE_GSTREAMER)    || \
-    defined(HAVE_QUICKTIME)    || \
-    defined(HAVE_QTKIT)        || \
-    defined(HAVE_AVFOUNDATION) || \
-    defined(HAVE_FFMPEG)       || \
-    defined(WIN32) /* assume that we have ffmpeg */
-
-#  define BUILD_WITH_VIDEO_INPUT_SUPPORT 1
-#else
-#  define BUILD_WITH_VIDEO_INPUT_SUPPORT 0
-#endif
+//
+//#include "test_precomp.hpp"
+//
+//#ifdef HAVE_CUDA
+//
+//using namespace cvtest;
+//
+//#if defined(HAVE_XINE)         || \
+//    defined(HAVE_GSTREAMER)    || \
+//    defined(HAVE_QUICKTIME)    || \
+//    defined(HAVE_QTKIT)        || \
+//    defined(HAVE_AVFOUNDATION) || \
+//    defined(HAVE_FFMPEG)       || \
+//    defined(WIN32) /* assume that we have ffmpeg */
+//
+//#  define BUILD_WITH_VIDEO_INPUT_SUPPORT 1
+//#else
+//#  define BUILD_WITH_VIDEO_INPUT_SUPPORT 0
+//#endif
 
 //////////////////////////////////////////////////////
 // MOG2
 
-#if BUILD_WITH_VIDEO_INPUT_SUPPORT
+//#if BUILD_WITH_VIDEO_INPUT_SUPPORT
+//
+//namespace
+//    {
+//IMPLEMENT_PARAM_CLASS(UseGray, bool)
+//    IMPLEMENT_PARAM_CLASS(DetectShadow, bool)
+//}
 
-namespace
-    {
-IMPLEMENT_PARAM_CLASS(UseGray, bool)
-    IMPLEMENT_PARAM_CLASS(DetectShadow, bool)
-}
-
-PARAM_TEST_CASE(MOG2, alvision.cuda::DeviceInfo, std::string, UseGray, DetectShadow, UseRoi)
+//PARAM_TEST_CASE(MOG2, alvision.cuda.DeviceInfo, std::string, UseGray, DetectShadow, UseRoi)
+class MOG2 extends alvision.cvtest.CUDA_TEST
 {
-    alvision.cuda::DeviceInfo devInfo;
-    std::string inputFile;
-    bool useGray;
-    bool detectShadow;
-    bool useRoi;
+    protected devInfo: alvision.cuda.DeviceInfo;
+    protected inputFile: string;
+    protected useGray: boolean;
+    protected detectShadow: boolean;
+    protected useRoi: boolean;
 
-    virtual void SetUp()
+    SetUp() : void
     {
-        devInfo = GET_PARAM(0);
-        alvision.cuda::setDevice(devInfo.deviceID());
+        this.devInfo = this.GET_PARAM<alvision.cuda.DeviceInfo>(0);
+        alvision.cuda.setDevice(this.devInfo.deviceID());
 
-        inputFile = std::alvision.cvtest.TS.ptr().get_data_path() + "video/" + GET_PARAM(1);
-        useGray = GET_PARAM(2);
-        detectShadow = GET_PARAM(3);
-        useRoi = GET_PARAM(4);
+        this.inputFile = alvision.cvtest.TS.ptr().get_data_path() + "video/" + this.GET_PARAM<string>(1);
+        this.useGray = this.GET_PARAM<boolean>(2);
+        this.detectShadow = this.GET_PARAM<boolean>(3);
+        this.useRoi = this.GET_PARAM<boolean>(4);
     }
 };
 
-CUDA_TEST_P(MOG2, Update)
+
+//CUDA_TEST_P(MOG2, Update)
+class MOG2_Update extends MOG2
 {
-    alvision.VideoCapture cap(inputFile);
-    ASSERT_TRUE(cap.isOpened());
+    public TestBody(): void {
+        var cap = new alvision.VideoCapture (this.inputFile);
+        alvision.ASSERT_TRUE(cap.isOpened());
 
-    alvision.Mat frame;
-    cap >> frame;
-    ASSERT_FALSE(frame.empty());
+        var frame = new alvision.Mat();
+        cap.read(frame);
+        alvision.ASSERT_FALSE(frame.empty());
 
-    alvision.Ptr<alvision.BackgroundSubtractorMOG2> mog2 = alvision.cuda::createBackgroundSubtractorMOG2();
-    mog2.setDetectShadows(detectShadow);
-    alvision.cuda::GpuMat foreground = createMat(frame.size(), CV_8UC1, useRoi);
+        var mog2 = alvision.cudabgsegm.createBackgroundSubtractorMOG2();
+        mog2.setDetectShadows(this.detectShadow);
+        var foreground = alvision.createMat(frame.size(), alvision.MatrixType.CV_8UC1, this.useRoi);
 
-    alvision.Ptr<alvision.BackgroundSubtractorMOG2> mog2_gold = alvision.createBackgroundSubtractorMOG2();
-    mog2_gold.setDetectShadows(detectShadow);
-    alvision.Mat foreground_gold;
+        var  mog2_gold = alvision.createBackgroundSubtractorMOG2();
+        mog2_gold.setDetectShadows(this.detectShadow);
+        var foreground_gold = new alvision.Mat();
 
-    for (int i = 0; i < 10; ++i)
-    {
-        cap >> frame;
-        ASSERT_FALSE(frame.empty());
-
-        if (useGray)
+        for (var i = 0; i < 10; ++i)
         {
-            alvision.Mat temp;
-            alvision.cvtColor(frame, temp, alvision.COLOR_BGR2GRAY);
-            alvision.swap(temp, frame);
-        }
+            cap.read(frame);
+            alvision.ASSERT_FALSE(frame.empty());
 
-        mog2.apply(loadMat(frame, useRoi), foreground);
+            if (this.useGray) {
+                var temp = new alvision.Mat();
+                alvision.cvtColor(frame, temp, alvision.ColorConversionCodes.COLOR_BGR2GRAY);
+                alvision.swap(temp, frame);
+            }
 
-        mog2_gold.apply(frame, foreground_gold);
+            mog2.apply(alvision.loadMat(frame, this.useRoi), foreground);
 
-        if (detectShadow)
-        {
-            ASSERT_MAT_SIMILAR(foreground_gold, foreground, 1e-2);
-        }
-        else
-        {
-            ASSERT_MAT_NEAR(foreground_gold, foreground, 0);
+            mog2_gold.apply(frame, foreground_gold);
+
+            if (this.detectShadow) {
+                alvision.ASSERT_MAT_SIMILAR(foreground_gold, foreground.getMat(), 1e-2);
+            }
+            else {
+                alvision.ASSERT_MAT_NEAR(foreground_gold, foreground, 0);
+            }
         }
     }
 }
 
-CUDA_TEST_P(MOG2, getBackgroundImage)
+//CUDA_TEST_P(MOG2, getBackgroundImage)
+class MOG2_getBackgroundImage extends MOG2
 {
-    if (useGray)
-        return;
+    public TestBody(): void {
+        if (this.useGray)
+            return;
 
-    alvision.VideoCapture cap(inputFile);
-    ASSERT_TRUE(cap.isOpened());
+        var cap = new alvision.VideoCapture (this.inputFile);
+        alvision.ASSERT_TRUE(cap.isOpened());
 
-    alvision.Mat frame;
+        var frame = new alvision.Mat();
 
-    alvision.Ptr<alvision.BackgroundSubtractorMOG2> mog2 = alvision.cuda::createBackgroundSubtractorMOG2();
-    mog2.setDetectShadows(detectShadow);
-    alvision.cuda::GpuMat foreground;
+        var mog2 = alvision.cudabgsegm.createBackgroundSubtractorMOG2();
+        mog2.setDetectShadows(this.detectShadow);
+        var foreground = new alvision.cuda.GpuMat();
 
-    alvision.Ptr<alvision.BackgroundSubtractorMOG2> mog2_gold = alvision.createBackgroundSubtractorMOG2();
-    mog2_gold.setDetectShadows(detectShadow);
-    alvision.Mat foreground_gold;
+        var mog2_gold = alvision.createBackgroundSubtractorMOG2();
+        mog2_gold.setDetectShadows(this.detectShadow);
+        var foreground_gold = new alvision.Mat();
 
-    for (int i = 0; i < 10; ++i)
-    {
-        cap >> frame;
-        ASSERT_FALSE(frame.empty());
+        for (var i = 0; i < 10; ++i)
+        {
+            cap.read(frame);
+            alvision.ASSERT_FALSE(frame.empty());
 
-        mog2.apply(loadMat(frame, useRoi), foreground);
+            mog2.apply(alvision.loadMat(frame, this.useRoi), foreground);
 
-        mog2_gold.apply(frame, foreground_gold);
+            mog2_gold.apply(frame, foreground_gold);
+        }
+
+        var background = alvision.createMat(frame.size(), frame.type(), this.useRoi);
+        mog2.getBackgroundImage(background);
+
+        var background_gold = new alvision.Mat();
+        mog2_gold.getBackgroundImage(background_gold);
+
+        alvision.ASSERT_MAT_NEAR(background_gold, background, 1);
     }
-
-    alvision.cuda::GpuMat background = createMat(frame.size(), frame.type(), useRoi);
-    mog2.getBackgroundImage(background);
-
-    alvision.Mat background_gold;
-    mog2_gold.getBackgroundImage(background_gold);
-
-    ASSERT_MAT_NEAR(background_gold, background, 1);
 }
 
-INSTANTIATE_TEST_CASE_P(CUDA_BgSegm, MOG2, testing::Combine(
-    ALL_DEVICES,
-    testing::Values(std::string("768x576.avi")),
-    testing::Values(UseGray(true), UseGray(false)),
-    testing::Values(DetectShadow(true), DetectShadow(false)),
-    WHOLE_SUBMAT));
-
-#endif
-
-#endif // HAVE_CUDA
+alvision.cvtest.INSTANTIATE_TEST_CASE_P('CUDA_BgSegm', 'MOG2', (test_case, test_name) => { return null; },new alvision.cvtest.Combine([
+    alvision.ALL_DEVICES,
+    ["768x576.avi"],
+    [true, false],
+    [true, false],
+    [alvision.WHOLE_SUBMAT]]));
+//
+//#endif
+//
+//#endif // HAVE_CUDA
