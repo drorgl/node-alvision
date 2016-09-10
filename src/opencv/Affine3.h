768920fc651ddcb7d90c941f5da33b9494577406
Dror Gluska <drorgl@yahoo.com>
2016-09-10 19:51:44 +0300

    start planning overload resolution.

--- src/opencv/Affine3.h
+++ src/opencv/Affine3.h
@@ -33,8 +33,34 @@ public:
 		info.GetReturnValue().Set(info.Holder());
 	}
 	
-
-	
+	//Identity() : Affine3<T>
+	//new() : Affine3<T>;
+	//new (affine: _matx.Matx<T>) : Affine3<T>;
+	//new (data: _mat.Mat, t ? : _matx.Vec<T> /*= Vec3::all(0)*/) : Affine3<T>;
+	//new (R: _matx.Matx<T>, t ? : _matx.Vec<T> /* = Vec3::all(0)*/) : Affine3<T>;
+	//new (rvec: _matx.Vec<T>, t ? : _matx.Vec<T> /* = Vec3::all(0)*/) : Affine3<T>;
+	//new (vals: Array<T>) : Affine3<T>;
+	//op_Multiplication(affine1: Affine3<T>, affine2 : Affine3<T>) : Affine3<T>;
+	//op_Multiplication(affine: Affine3<T>, vector : Array<T>) : Affine3<T>;
+	//op_Multiplication(affine: Affine3<_st.double>, vector : _matx.Vec3d) : _matx.Vec3d;
+	//op_Multiplication(affine: Affine3<_st.float>, vector : _matx.Vec3f) : _matx.Vec3f;
+	//
+	//
+	//concatenate(affine : Affine3<T>) : Affine3<T>;
+	//inv(method ? : _base.DecompTypes /*= cv::DECOMP_SVD*/) : Affine3<T>;
+	//linear() : _matx.Matx<T>;
+	//linear(L : _matx.Matx<T>) : void;
+	//matrix: _matx.Matx<T>;
+	//rotate(R : _matx.Matx<T>) : Affine3<T>;
+	//rotate(rvec : _matx.Vec3<T>) : Affine3<T>;
+	//rotation() : _matx.Matx<T>;
+	//rotation(data : _mat.Mat) : void;
+	//rotation(R : _matx.Matx<T>) : void;
+	//rotation(rvec : _matx.Vec3<T>) : void;
+	//rvec() : _matx.Vec<T>;
+	//translate(t : _matx.Vec3<T>) : Affine3<T>;
+	//translation() : _matx.Vec<T>;
+	//translation(t : _matx.Vec<T>) : void;
 };
 
 
